ce430635082e378e9218070ddee1f5606ed48f04
Jared Rickert <jaredrickert52@gmail.com>
2025-07-12 10:49:04 -0500

    feat(keymaps): Add file explorer toggle keymap
    Add a keybinding `<leader>e` in normal mode.
    
    This allows users to easily toggle the Neotree file
    explorer, starting in the current working directory.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,6 +8,11 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- File explorer
+vim.keymap.set('n', '<leader>e', function()
+	require('neo-tree.command').execute({ toggle = true, dir = vim.fn.getcwd() })
+end, { desc = '[E]xplore files' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
